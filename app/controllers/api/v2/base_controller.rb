7d4c03b4a08483d977eae992b8a80343875d8147
Bunlong <obunlong@gmail.com>
2018-07-24 10:07:34 +0700

    fixed fetching form

--- app/controllers/api/v2/base_controller.rb
+++ app/controllers/api/v2/base_controller.rb
@@ -28,37 +28,11 @@ class API::V2::BaseController < ApplicationController
 
   private
 
-  # Handles errors to give nice JSON response
   def handle_error(exception, options = {})
     if exception.is_a?(ActiveRecord::RecordNotFound)
       render json: { errors: [exception.message.downcase.gsub(" ", "_")] }
     else
       raise exception
     end
-  end
-
-  def find_form
-    if params[:form_id].blank?
-      render json: { errors: ["form_id_required"] }, status: 422
-    else
-      @form = Form.where(id: params[:form_id]).includes(:whitelistings).first
-
-      if @form.nil?
-        return render json: { errors: ["form_not_found"] }, status: 404
-      elsif !@form.api_user_id_can_see?(@api_user.id)
-        return render json: { errors: ["access_denied"] }, status: 403
-      end
-    end
-  end
-
-  # Applies created_before/after filter to Answers or Responses
-  def add_date_filter(objects)
-    if params[:created_after].present?
-      objects = objects.created_after(Time.zone.parse(params[:created_after]))
-    end
-    if params[:created_before].present?
-      objects = objects.created_before(Time.zone.parse(params[:created_before]))
-    end
-    objects
   end
 end
--- app/controllers/api/v2/forms_controller.rb
+++ app/controllers/api/v2/forms_controller.rb
@@ -1,6 +1,6 @@
 class API::V2::FormsController < API::V2::BaseController
   def index
-    forms = @api_user.last_mission.forms
+    forms = current_mission.forms
     render json: forms, each_serializer: API::V2::FormSerializer
   end
 end
