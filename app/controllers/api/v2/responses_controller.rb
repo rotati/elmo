58c1c1a597d8981ddda69263211ba35bf3956ad5
Bunlong <obunlong@gmail.com>
2018-08-17 11:28:34 +0700

    revert to default response params

--- app/controllers/api/v2/responses_controller.rb
+++ app/controllers/api/v2/responses_controller.rb
@@ -16,13 +16,30 @@ class API::V2::ResponsesController < API::V2::BaseController
   private
 
   def response_params
-    params.require(:response).permit(:form_id, :user_id, :incomplete, answers_attributes: [
-      :id, :value, :option_id, :option_node_id, :questioning_id, :relevant, :rank,
-      :inst_num, :media_object_id, :_destroy, :'time_value(1i)', :'time_value(2i)', :'time_value(3i)',
-      :'time_value(4i)', :'time_value(5i)', :'time_value(6i)', :datetime_value, :'datetime_value(1i)',
-      :'datetime_value(2i)', :'datetime_value(3i)', :'datetime_value(4i)', :'datetime_value(5i)',
-      :'datetime_value(6i)', :date_value, :'date_value(1i)', :'date_value(2i)', :'date_value(3i)',
-      choices_attributes: [ :id, :option_id, :option_node_id, :checked ]
-    ])
+    if params[:response]
+      to_permit = [:form_id, :user_id, :incomplete]
+
+      if (ans_attribs = params[:response][:answers_attributes]) &&
+          (action_name != "update" || can?(:modify_answers, @response))
+
+        to_permit << {
+          answers_attributes: ans_attribs.keys.map { |k| [k, permitted_answer_attributes] }.to_h
+        }
+      end
+
+      params.require(:response).permit(to_permit)
+    end
+  end
+
+  def permitted_answer_attributes
+    @permitted_answer_attributes ||= %w(
+      id value option_id option_node_id questioning_id relevant rank
+      time_value(1i) time_value(2i) time_value(3i) time_value(4i) time_value(5i) time_value(6i)
+      datetime_value
+      datetime_value(1i) datetime_value(2i) datetime_value(3i)
+      datetime_value(4i) datetime_value(5i) datetime_value(6i)
+      date_value(1i) date_value(2i) date_value(3i) inst_num media_object_id _destroy
+      date_value
+    ) << {choices_attributes: %i[id option_id option_node_id checked]}
   end
 end
