980309a8d675954fa0cf456224c67cbbf59aef5c
Bunlong <obunlong@gmail.com>
2018-08-17 10:51:37 +0700

    fixed create response api

--- app/controllers/api/v2/responses_controller.rb
+++ app/controllers/api/v2/responses_controller.rb
@@ -16,48 +16,27 @@ class API::V2::ResponsesController < API::V2::BaseController
   private
 
   def response_params
-    if params[:response]
-      reviewer_only = if @response.present?
-        [:reviewed, :reviewer_notes, :reviewer_id]
-      else
-        []
-      end
+      if params[:response]
+        to_permit = [:form_id, :user_id, :incomplete]
 
-      params.require(:response).permit(:form_id, :user_id, :incomplete, *reviewer_only).tap do |permitted|
-        # In some rare cases, create or update can occur without answers_attributes. Not sure how.
-        # Also need to respect the modify_answers permission here.
-        if params[:response][:answers_attributes] &&
-          (action_name != "update" || can?(:modify_answers, @response))
-          permit_answer_attributes(permitted)
+        if (ans_attribs = params[:response][:answers_attributes])
+          to_permit << {
+            answers_attributes: ans_attribs.keys.map { |k| [k, permitted_answer_attributes] }.to_h
+          }
         end
+        params.require(:response).permit(to_permit)
       end
     end
-  end
 
-  def permit_answer_attributes(permitted)
-    permitted[:answers_attributes] = {}
-
-    # The answers_attributes hash might look like {'2746' => { ... }, '2731' => { ... }, ... }
-    # The keys are irrelevant so we permit all of them, but we only want to permit certain attribs
-    # on the answers.
-    permitted_answer_attribs = %w(id value option_id option_node_id questioning_id relevant rank
-      time_value(1i) time_value(2i) time_value(3i) time_value(4i) time_value(5i) time_value(6i)
-      datetime_value
-      datetime_value(1i) datetime_value(2i) datetime_value(3i)
-      datetime_value(4i) datetime_value(5i) datetime_value(6i)
-      date_value(1i) date_value(2i) date_value(3i) inst_num media_object_id _destroy
-      date_value)
-
-    params[:response][:answers_attributes].each do |idx, attribs|
-      permitted[:answers_attributes][idx] = attribs.permit(*permitted_answer_attribs)
-      # Handle choices, which are nested under answers.
-      if attribs[:choices_attributes]
-        permitted[:answers_attributes][idx][:choices_attributes] = {}
-        attribs[:choices_attributes].each do |idx2, attribs2|
-          permitted[:answers_attributes][idx][:choices_attributes][idx2] = attribs2.permit(
-            :id, :option_id, :option_node_id, :checked)
-        end
-      end
+    def permitted_answer_attributes
+      @permitted_answer_attributes ||= %w(
+        id value option_id option_node_id questioning_id relevant rank
+        time_value(1i) time_value(2i) time_value(3i) time_value(4i) time_value(5i) time_value(6i)
+        datetime_value
+        datetime_value(1i) datetime_value(2i) datetime_value(3i)
+        datetime_value(4i) datetime_value(5i) datetime_value(6i)
+        date_value(1i) date_value(2i) date_value(3i) inst_num media_object_id _destroy
+        date_value
+      ) << {choices_attributes: %i[id option_id option_node_id checked]}
     end
-  end
 end
