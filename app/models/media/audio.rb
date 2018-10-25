c106bdbc82f16bfd351eafa2f1d3e417d6154261
Bunlong <obunlong@gmail.com>
2018-10-25 16:09:41 +0700

    allow audio to support aac

--- app/models/media/audio.rb
+++ app/models/media/audio.rb
@@ -3,7 +3,7 @@
 class Media::Audio < Media::Object
   # For some reason, the mime-magic gem returns video/ogg for audio OGG files. Same for WEBM.
   validates_attachment_content_type :item, content_type: [%r{\Aaudio/.*\Z}, "video/ogg", "video/webm"]
-  validates_attachment_file_name :item, matches: /\.(mp3|ogg|webm|wav)\Z/i
+  validates_attachment_file_name :item, matches: /\.(mp3|ogg|webm|wav|aac)\Z/i
 
   def thumb_path
     "media/audio.png"
